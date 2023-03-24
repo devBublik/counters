ba7e21369eabeb1355491c2893186452be14aeb0
e.bublikova <e.bublikova@cinet.ru>
2023-03-24 16:34:32 +0300

    init project

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div class="counter">
+        <div class="counter__display">2:40</div>
+        <div class="counter__controls">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import IconPlus from '../components/icons/IconPlus.vue';
+export default {
+    name: 'Counter',
+    components: {
+        IconPlus: 
+    }
+}
+</script>
+
+<style>
+
+</style>
